760cab311922c7af6a0915c7d6e5c132e6b29e11
MeAkash77 <itsaakash77@gmail.com>
2025-07-30 00:46:29 +0530

    first commit

--- crypto/src/utils/firebaseConfig.ts
+++ crypto/src/utils/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6FTYPFv48pIi9Gx53lWmPp4vZWwjKL98",
-  authDomain: "academicpal-706ff.firebaseapp.com",
-  projectId: "academicpal-706ff",
-  storageBucket: "academicpal-706ff.firebasestorage.app",
-  messagingSenderId: "890532166738",
-  appId: "1:890532166738:web:1df65e4d2b2c6ce0bff38b",
-  measurementId: "G-JXN7W3VSXW"
+  apiKey: "AIzaSyDSa6WbMn7bSjw_ddH_CfN0hi3wQJxXpoI",
+  authDomain: "ecommerce77-f06f0.firebaseapp.com",
+  projectId: "ecommerce77-f06f0",
+  storageBucket: "ecommerce77-f06f0.firebasestorage.app",
+  messagingSenderId: "235844479074",
+  appId: "1:235844479074:web:33817742087c3d31b6893b",
+  measurementId: "G-1PDRJR0SJB"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
